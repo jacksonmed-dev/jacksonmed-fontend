d88329ea1ffc2008ceeb837d409939a1e8d8018a
Ian <85652869+jmsian@users.noreply.github.com>
2022-12-14 16:19:05 -0700

    restructuring the storyboard

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-//import 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
 
 import 'getframe.dart';
 import 'paintsensor.dart';
@@ -22,27 +22,68 @@ class MyApp extends StatelessWidget {
       title: _title,
       theme: ThemeData(
         brightness: Brightness.dark,
-        primaryColor: Colors.blue,
       ),
-      home: const OracleDemo(),
+      home: const FacilityView(),
     );
   }
 }
 
-class OracleDemo extends StatefulWidget {
-  const OracleDemo({super.key});
+//FACILITY VIEW
+
+class FacilityView extends StatefulWidget {
+  const FacilityView({super.key});
 
   @override
-  State<OracleDemo> createState() => _OracleDemoState();
+  State<FacilityView> createState() => _FacilityViewState();
 }
 
-class _OracleDemoState extends State<OracleDemo> {
-  int currentPageIndex = 0;
-  String url = 'http://192.168.1.3/api/frames';
-  String apiurl = 'https://test.api.jacksonmed.org/patient';
+class _FacilityViewState extends State<FacilityView> {
+  final facilityName = 'Jackson Medical Solutions';
+  List<Patient> patient = [];
 
-//  List<Patient> patient = [];
-  List<int> frame = List<int>.filled(1728, 0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('$facilityName Overview')),
+      ),
+      body: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const PatientForm();
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+//FACILITY VIEW
+//PATIENT FORM PAGE
+
+class PatientForm extends StatefulWidget {
+  const PatientForm({super.key});
+
+  @override
+  State<PatientForm> createState() => _PatientFormState();
+}
+
+class _PatientFormState extends State<PatientForm> {
+  final _patientFormKey = GlobalKey<FormState>();
+  final fnameController = TextEditingController();
+  final lnameController = TextEditingController();
+  final bdayController = TextEditingController();
+  final heightController = TextEditingController();
+  final weightController = TextEditingController();
+  final sexController = TextEditingController();
+
+  final apiurl = 'https://test.api.jacksonmed.org/patient';
 
   Patient patient = Patient(
     fname: '',
@@ -52,20 +93,6 @@ class _OracleDemoState extends State<OracleDemo> {
     weight: '',
     sex: '',
   );
-
-  final fnameController = TextEditingController();
-  final lnameController = TextEditingController();
-  final bdayController = TextEditingController();
-  final heightController = TextEditingController();
-  final weightController = TextEditingController();
-  final sexController = TextEditingController();
-
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   void dispose() {
@@ -78,6 +105,160 @@ class _OracleDemoState extends State<OracleDemo> {
     super.dispose();
   }
 
+  Future<http.Response> createPatient(Patient p) {
+    return http.post(
+      Uri.parse(apiurl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "firstName": p.fname,
+        "lastName": p.lname,
+        "birthday": p.bday,
+        "height": p.height,
+        "weight": p.weight,
+        "sex": p.sex
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('New Patient')),
+      ),
+      body: Form(
+        key: _patientFormKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: fnameController,
+              decoration: const InputDecoration(
+                labelText: 'First Name',
+                hintText: 'ex: Anakin',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: lnameController,
+              decoration: const InputDecoration(
+                labelText: 'Last Name',
+                hintText: 'ex: Skywalker',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: bdayController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.calendar_today),
+                labelText: 'Date of Birth',
+              ),
+              readOnly: true,
+              onTap: () async {
+                FocusScope.of(context).requestFocus(FocusNode());
+                await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                );
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: heightController,
+              decoration: const InputDecoration(
+                hintText: 'Height',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: weightController,
+              decoration: const InputDecoration(
+                hintText: 'Weight',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: sexController,
+              decoration: const InputDecoration(
+                hintText: 'Sex',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Invalid';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_patientFormKey.currentState!.validate()) {
+                  patient.fname = fnameController.text;
+                  patient.lname = lnameController.text;
+                  patient.bday = bdayController.text;
+                  patient.height = heightController.text;
+                  patient.weight = weightController.text;
+                  patient.sex = sexController.text;
+                  createPatient(patient);
+                }
+              },
+              child: const Text('Save & Continue'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//PATIENT FORM
+//ORACLE DEMO
+
+class OracleDemo extends StatefulWidget {
+  const OracleDemo({super.key});
+
+  @override
+  State<OracleDemo> createState() => _OracleDemoState();
+}
+
+class _OracleDemoState extends State<OracleDemo> {
+  int currentPageIndex = 0;
+  String url = 'http://192.168.1.3/api/frames';
+
+  List<int> frame = List<int>.filled(1728, 0);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   void getFrame() async {
     var response = await http.get(Uri.parse(url));
     Frame r = frameFromJson(response.body);
@@ -85,23 +266,6 @@ class _OracleDemoState extends State<OracleDemo> {
     setState(() {
       frame = r.readings.toList();
     });
-  }
-
-  Future<http.Response> createPatient(Patient pat) {
-    return http.post(
-      Uri.parse(apiurl),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        "firstName": pat.fname,
-        "lastName": pat.lname,
-        "birthday": pat.bday,
-        "height": pat.height,
-        "weight": pat.weight,
-        "sex": pat.sex
-      }),
-    );
   }
 
   @override
@@ -141,108 +305,9 @@ class _OracleDemoState extends State<OracleDemo> {
             ),
           ),
         ),
-        Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: fnameController,
-                decoration: const InputDecoration(
-                  hintText: 'First Name',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: lnameController,
-                decoration: const InputDecoration(
-                  hintText: 'Last Name',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: bdayController,
-                decoration: const InputDecoration(
-//                  labelText: 'Date of Birth',
-//                  hintText: 'Birthdate',
-                    ),
-                onTap: () async {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                  await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(1900),
-                    lastDate: DateTime(2100),
-                  );
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: heightController,
-                decoration: const InputDecoration(
-                  hintText: 'Height',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: weightController,
-                decoration: const InputDecoration(
-                  hintText: 'Weight',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: sexController,
-                decoration: const InputDecoration(
-                  hintText: 'Sex',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Invalid';
-                  }
-                  return null;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    patient.fname = fnameController.text;
-                    patient.lname = lnameController.text;
-                    patient.bday = bdayController.text;
-                    patient.height = heightController.text;
-                    patient.weight = weightController.text;
-                    patient.sex = sexController.text;
-                    createPatient(patient);
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ],
-          ),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Page 2'),
         ),
         Container(
           alignment: Alignment.center,
